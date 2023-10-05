16c9d4c7eefb7b8656f596e2a25ccf9da3324586
dianamongar <dianaaneliz@gmail.com>
2023-10-05 01:04:10 -0400

    listado de etiquetas en cards

--- src/components/TagCrud.vue
+++ src/components/TagCrud.vue
@@ -1,10 +1,32 @@
 <template>
     <NavBarBase/>
-    <h1>hola :D </h1>
-    <DataTable :value="tags" removableSort tableStyle="min-width: 50rem">
-        <ColumnComponent field="" header="N°" sortable style="width: 25%"></ColumnComponent>
-        <ColumnComponent field="name" header="Name" sortable style="width: 25%"></ColumnComponent>
-    </DataTable>
+    <div class="cabecera">
+        <h1>Lista de etiquetas</h1>
+        <div>
+            <ButtonComponent @click="createTag" class="p-button p-button-primary"><strong>Nueva etiqueta</strong></ButtonComponent>
+        </div> 
+    </div>
+    <br/>
+    <div class="list-group" style="padding:5%" >
+                <div v-for="tag in tags" :key="tag.id_tags" class="card" >
+                    <div class="card-body">
+                        <a href="#" class="list-group-item list-group-item-action ">
+                            <div>
+                                <div class="d-flex w-100 justify-content-between">
+                                    <h5 class="mb-1">{{ tag.name }}</h5>
+                                    
+                                </div>
+                            </div>
+                        </a>
+                        <br/>
+                        <div class="d-flex w-100 justify-content-between">
+                            <a href="#" class="btn btn-primary btn-sm mr-2" ><i class="fas fa-pencil-alt"></i> Editar</a>
+                            <a href="#" class="btn btn-danger btn-sm"  @click="() => confirmDelete(task.task.id_tasks)"><i class="fas fa-trash-alt"></i> Borrar</a>
+                        </div>
+                    </div>
+                </div>
+            <br/>
+        </div>
 </template>
 <script>
 import TagService from '../service/TagService';
